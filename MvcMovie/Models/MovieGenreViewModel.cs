d1464e35fcf7547493149075342cee76ec84e86a
ddmartin3 <darwood.martin@gmail.com>
2018-08-11 18:46:45 -0700

    first commit

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
